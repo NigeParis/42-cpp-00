7619ab26179abf459ec1f34d8aec634eba8345a2
NigeParis <nrobinso@student.42.fr>
2024-12-04 15:24:33 +0100

    ex00 done to be checked

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 42-cpp-00
+
+
+
+
+ref : https://cplusplus.com/reference/
+
+	: https://en.cppreference.com/w/
---
+++ ex00/src/MegaPhone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 14:48:17 by nrobinso          #+#    #+#             */
+/*   Updated: 2024/12/04 14:55:02 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+
+	if (argc > 1) {
+	
+		for (int i = 1; i < argc; i++) {
+			std :: string str = argv[i];
+	
+			for (int i = 0; str[i]; i++) {
+				str[i] = std :: toupper((int)str[i]);
+			}
+			std :: cout << str;
+		}
+		std :: cout << std :: endl;
+	}
+	else 
+		std :: cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std :: endl;
+	return (0);
+}
